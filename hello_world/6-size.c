b27380aa7eec64424c5f6ea421049a28592da269
ArioCanario <nahuelmt24@gmail.com>
2022-09-27 10:02:18 -0700

    Task-6

---
+++ hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - This is a description
+ * Return:0
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)", sizeof(char));
+	printf("Size of an int: %zu byte(s)", sizeof(int));
+	printf("Size of a long int: %zu byte(s)", sizeof(int));
+	printf("Size of a long long int: %zu byte(s)", sizeof(int));
+	printf("Size of a float: %zu byte(s)", sizeof(float));
+	return (0);
+}
