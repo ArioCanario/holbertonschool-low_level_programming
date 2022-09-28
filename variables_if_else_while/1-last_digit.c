9e274eada21c9cd5968f645a442f416095764173
ArioCanario <nahuelmt24@gmail.com>
2022-09-28 10:01:36 -0700

    Task-1

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - This is a description
+ * Return: 0
+*/
+
+int main(void)
+{
+		int n;
+
+		srand(time(0));
+		n = rand() - RAND_MAX / 2;
+		/* your code goes there */
+		if ((n % 10) > 5)
+		{
+			printf("Last digit of %d is %d andis greater than 5", n, n % 10);
+	}
+		else if (((n % 10) < 6) && ((n % 10) != 0));
+		{
+			printf("Last digit of %d is %d and is less than 6 and not 0", n, n % 10);
+		}
+		
+			printf("Last digit of %d is %d and is 0", n, n % 10);
+		
+		return (0);
+}
