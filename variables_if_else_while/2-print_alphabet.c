e0351e545fcb943f5dd0522be02982da4bb42be3
ArioCanario <nahuelmt24@gmail.com>
2022-09-28 15:02:18 -0700

    Task-2-Fix

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - This is a description
+ * Return: 0
+ */
+
+int main(void)
+	{
+		char c;
+
+		for (c = 'a'; c < 'z'; c++)
+		{
+			putchar(c);
+		}
+		if (c == 'z')
+		{
+			putchar(c);
+		}
+			return (0);
+}
