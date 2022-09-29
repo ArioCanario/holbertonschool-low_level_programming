474a26ee6df9b108ebf63388d7a4c1d73a298776
NMenendez24 <nahuelmt24@gmail.com>
2022-09-29 16:54:06 -0700

    Task-1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "_putchar.c"
+
+/**
+ * main - This is a description
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
