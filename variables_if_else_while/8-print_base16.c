b2054a09591c6d20ccd6e0cae158e28ce16dd98e
ArioCanario <nahuelmt24@gmail.com>
2022-09-29 07:43:23 -0700

    Task-8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - This is a description
+ * Return: 0
+ */
+int main(void)
+{
+	char h;
+
+	for (h = 48; h <= 57; h++)
+	{
+		putchar(h);
+	}
+	for (h = 97; h <= 102; h++)
+	{
+		putchar(h);
+	}
+	putchar('\n');
+	return (0);
+}
