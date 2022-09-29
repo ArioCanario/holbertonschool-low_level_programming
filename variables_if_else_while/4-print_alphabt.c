7f44e868166473b5c790223e478ba6fe85bbf5ae
ArioCanario <nahuelmt24@gmail.com>
2022-09-29 05:20:44 -0700

    Task-4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - This is a description
+ * Return: 0
+ */
+
+int main(void)
+	{
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			if (c == 'q' || c == 'e')
+			{
+				c++;
+			}
+			else
+			{
+				putchar(c);
+			}}
+			putchar('\n');
+			return (0);
+}
