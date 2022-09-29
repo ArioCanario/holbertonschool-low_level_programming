877f877ebfd0b9e06b4748fefb27ecf370b747b3
ArioCanario <nahuelmt24@gmail.com>
2022-09-29 08:34:20 -0700

    Task-9-Fix

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -11,10 +11,12 @@ int main(void)
 	for (n = 48; n <= 57; n++)
 	{
 		putchar(n);
-	if (n < 57)
-		putchar(',');
-		putchar(' ');
-	}
+		if (n < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	putchar('\n');
 	return (0);
+}
 }
