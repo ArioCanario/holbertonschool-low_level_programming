5519654f3b96207db3f977a6bd1884dca25bb6db
ArioCanario <nahuelmt24@gmail.com>
2022-09-29 07:00:35 -0700

    task-6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -4,13 +4,14 @@
  * main - This is a description
  * Return: 0
  */
-int main(void)
-{
-	char n;
 
-	for (n = 48; n <= 57; n++)
+	int main(void)
 	{
-		putchar(n);
-	}
+		char n;
+
+		for (n = 48; n <= 57; n++)
+		{
+			putchar(n);
+		}
 	putchar('\n');
-}
+	}
