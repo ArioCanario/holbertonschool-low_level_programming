807c57c5507c1c867073794a8d0242fdd2345af1
NMenendez24 <nahuelmt24@gmail.com>
2022-09-30 09:34:22 -0700

    Task-3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,8 +1,13 @@
 #include "main.h"
 
+/**
+ * _islower - Check if its lowercase
+ * Return: 0
+ *@c: Is a variable
+ */
 int _islower(int c)
 {
-        if (c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
