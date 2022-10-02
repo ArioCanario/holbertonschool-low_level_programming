be317fec2254d69a0a12fe7e4700df8c4e61d2f7
NMenendez24 <nahuelmt24@gmail.com>
2022-10-02 14:43:25 -0300

    Task-7-Fix-2

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -3,13 +3,12 @@
 /**
  * print_last_digit - Print the last digit of a number
  * Return: c
- * @c: Last digit of given number
  * @n: Given number
  */
 
 int print_last_digit(int n)
 {
-	int c = n % 10;
+	int c = (n % 10);
 
 	if (c < 0)
 	{
