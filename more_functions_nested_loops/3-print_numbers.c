7e726a5208fe4c1a8d50ebc664b8cd4031441b1e
NMenendez24 <nahuelmt24@gmail.com>
2022-10-03 11:38:10 -0700

    Task-3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Pritns the numbers from 0 to 9
+ * Return: Nothing
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		_putchar(c + '0');
+	}
+	_putchar('\n');
+}
