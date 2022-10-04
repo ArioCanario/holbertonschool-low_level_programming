4eb33be9256d5931d95a9525168e82c438467608
NMenendez24 <nahuelmt24@gmail.com>
2022-10-03 21:07:15 -0300

    Task-6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - Prints a line of the given length
+ * Return: Nothing
+ *@n: Length of th line
+ */
+
+void print_line(int n)
+{
+	int c;
+
+	if (n > 0)
+	{
+		for (c = 0; c < n; c++)
+		{
+			_putchar('_');
+		}
+		_putchar(10);
+	}
+	_putchar(10);
+}
