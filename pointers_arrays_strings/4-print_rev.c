e61bee12aacfe0299039c8c05dfc78760b810cb9
NMenendez24 <nahuelmt24@gmail.com>
2022-10-06 11:02:48 -0700

    Task-4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string
+ * Return: nothing
+ * @s: Name of the string
+ */
+
+void print_rev(char *s)
+{
+	int l = 0;
+
+	while (s[l] != '\0')
+	{
+		l++;
+	}
+	while (l >= 0)
+	{
+		_putchar(s[l]);
+		l--;
+	}
+	_putchar(10);
+}
