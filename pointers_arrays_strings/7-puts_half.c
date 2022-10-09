3b2a4e4163e8321a450a609b6c69afabf7d2444e
NMenendez24 <nahuelmt24@gmail.com>
2022-10-09 17:08:26 -0300

    Task-7Fix

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -16,7 +16,7 @@ void puts_half(char *str)
 		l++;
 	}
 	l--;
-		for (i = (l / 2); (i < l && (i % 2) != 0) || ((i % 2) == 0); i++)
+		for (i = (l / 2); (i <= l && (i % 2) != 0) || ((i % 2) == 0); i++)
 		{
 			_putchar(str[i]);
 		}
