bcf99afa58af7b139122df7746b64f61d9fe45f7
NMenendez24 <nahuelmt24@gmail.com>
2022-10-10 20:07:07 -0300

    Task-1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+	int i = 0;
+	int j = 0;
+
+	for (; dest[i] != '\0'; i++)
+	{
+	}
+	if (n < i)
+	{
+		for (j = 0; j < n; j++)
+		{
+			dest[i] = src[j];
+			i++;
+		}
+	}
+	else if ((n > i) && (i >= 0))
+	{
+		for (j = 0; j < n; j++)
+		{
+			dest[i] = src[j];
+			i++;
+		}
+	}
+	return (dest);
+}
