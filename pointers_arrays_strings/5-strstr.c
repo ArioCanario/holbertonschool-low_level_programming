99ee916b051c00050d1c21c746463d9c7296e36c
NMenendez24 <nahuelmt24@gmail.com>
2022-10-13 10:08:18 -0700

    Task-5-Fix

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -13,7 +13,11 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		if (needle[0] == haystack[i] && needle[1] == haystack[i + 1])
+		if (needle[0] == '\0')
+		{
+			return (NULL)
+		}
+		else if (needle[0] == haystack[i] && needle[1] == haystack[i + 1])
 		{
 			return (&haystack[i]);
 		}
