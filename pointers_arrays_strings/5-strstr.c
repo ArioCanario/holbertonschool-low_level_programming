bc304d01ff29c3f2101a3622438de41cdcad2c8d
NMenendez24 <nahuelmt24@gmail.com>
2022-10-13 10:05:29 -0700

    Task-5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -13,7 +13,7 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		if (needle[0] == haystack[i])
+		if (needle[0] == haystack[i] && needle[1] == haystack[i + 1])
 		{
 			return (&haystack[i]);
 		}
