38cfe62643471c6e1699265200a4eaceb553c8bc
NMenendez24 <nahuelmt24@gmail.com>
2022-10-13 09:16:25 -0700

    Task-3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.c"
+
+/**
+ * _strspn - Gets the length of a prefix substring
+ * @s: Pointer
+ * @accept: Pointer
+ * Return: count
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, h;
+	unsigned int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		h = 1;
+
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				count++;
+				h = 0;
+			}
+		}
+		if (h == 1)
+			break;
+	}
+	return (count);
+}
