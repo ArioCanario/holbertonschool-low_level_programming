033f6f267f47b509e7f4ba7c8745881d499aac5b
NMenendez24 <nahuelmt24@gmail.com>
2022-10-13 06:23:56 -0700

    Task-2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strchr - Copies the first n bytes with b
+ * @s: Pointer
+ * @n: Bytes number
+ * Return: Pointer to the character
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0;s[i] != '\0'; i++)
+	{
+	
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+	if (s[i] == c)
+		return (&s[i]);
+
+	return (NULL);
+}
