2ea3d2e7df03308b36553d422567d0100926bcb4
NMenendez24 <nahuelmt24@gmail.com>
2022-10-17 05:41:47 -0700

    Task-3-BettyFix

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,10 +1,16 @@
 #include "main.h"
 
+/**
+ * factorial - Calculates the factorial of a number
+ * Return: Result
+ * @n: Number
+ */
+
 int factorial(int n)
 {
 	if (n == 0)
 	{
-		return(1);
+		return (1);
 	}
 	else if (n < 0)
 	{
