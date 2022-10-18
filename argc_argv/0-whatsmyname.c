e24b5135bcf80c8bff867af7d6551a039cc25b87
NMenendez24 <nahuelmt24@gmail.com>
2022-10-18 06:37:42 -0700

    Task-0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file
+ * Return: 0
+ * @argc: argc
+ * @argv: argv
+ */
+
+int main(int argc, char **argv)
+{
+	printf("./%s", argv[8]);
+	(void)argc;
+	return (0);
+}
