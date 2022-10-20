c0b5f9adb9ec7a81082e04a2c55d9428b6d30d60
NMenendez24 <nahuelmt24@gmail.com>
2022-10-20 11:08:42 -0700

    Task-2-Fix

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -6,6 +6,16 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j, k, l;
 	char *conc;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 	}
@@ -14,7 +24,7 @@ char *str_concat(char *s1, char *s2)
 	{
 	}
 
-	conc = malloc(sizeof(char) * (i + j));
+	conc = malloc(sizeof(char) * (i + j) + 1);
 
 	for (k = 0; k < i; k++)
 	{
@@ -23,6 +33,10 @@ char *str_concat(char *s1, char *s2)
 	for (l = 0; (k + l) < (i + j); l++)
 	{
 		conc[k + l] = s2[l];
+	}
+	if (conc == NULL)
+	{
+		return (NULL);
 	}
 	return (conc);
 }
