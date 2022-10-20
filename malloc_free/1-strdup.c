ebf28b7f44345740b93e2f29be7eb0ef1f543d5c
NMenendez24 <nahuelmt24@gmail.com>
2022-10-20 07:52:52 -0700

    Task-1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+char *_strdup(char *str)
+{
+	unsigned int i, j;
+	char *nstr;
+
+	if(str == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; str[j] != '\0'; j++)
+	{
+	}
+	if (j < 1)
+	{
+		return(NULL);
+	}
+
+	nstr = malloc(sizeof(char) * j);
+
+
+	for (i = 0; i < j; i++)
+	{
+		nstr[i] = str[i];
+	}
+	return (nstr);
+
+}
