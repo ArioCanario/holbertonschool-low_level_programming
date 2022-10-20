078efe2d48c8d1e01bac57e9dc4dd7ba478c52c8
NMenendez24 <nahuelmt24@gmail.com>
2022-10-20 07:05:41 -0700

    Task-0-Fix4

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -14,19 +14,21 @@ char *create_array(unsigned int size, char c)
 	char *str;
 	unsigned int i;
 
-	str = malloc(sizeof(char) * size);
-
-	for (i = 0; size > i; i++)
-	{
-		str[i] = c;
-	}
 	if (size < 1)
 	{
 		return (NULL);
 	}
+
+	str = malloc(sizeof(char) * size);
+
 	if (str == NULL)
 	{
 		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		str[i] = c;
 	}
 	return (str);
 }
