2233ce243e2f73bbe7fe9e8b3880333b95c29e44
NMenendez24 <nahuelmt24@gmail.com>
2022-10-21 06:53:39 -0700

    Task-0-Fix

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,20 +1,26 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * malloc_checked - Save memory depending of the type
+ * Return: ptr on success, 98 on fail
+ * @b: Length of the array
+ */
+
 void *malloc_checked(unsigned int b)
 {
-	int *ptr;
+	void *ptr;
 
 	if (b < 1)
 	{
-		return (NULL);
+		exit(98);
 	}
 
-	ptr = malloc(sizeof(int) * b);
+	ptr = malloc(sizeof(*ptr) * b);
 
 	if (ptr == NULL)
 	{
-		return (NULL);
+		exit(98);
 	}
 	return (ptr);
 }
