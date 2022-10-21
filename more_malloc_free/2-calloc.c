1d4aa9e0e4109b3dc077ca023970af88dfab44dd
NMenendez24 <nahuelmt24@gmail.com>
2022-10-21 08:13:42 -0700

    Task-2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - Alocates memory and set it in 0
+ * Return: Pointer on success, NULL if it fails
+ * @nmemb: Length of the array to allocate
+ * @size: Size of the members
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i;
+	int *ptr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < nmemb; i++)
+	{
+		ptr[i] = 0;
+	}
+	return (ptr);
+}
--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -8,5 +8,6 @@ char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif
