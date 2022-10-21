4435c532741196a0ce4facd66cdc11686a27a165
NMenendez24 <nahuelmt24@gmail.com>
2022-10-21 07:59:10 -0700

    Task-1-BettyFix

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * str_concat - Concatenates 2 strings
+ * string_nconcat - Concatenates 2 strings
  * Return: NULL if the program fails, conc in success
  * @s1: First string
  * @s2: Second string
@@ -18,32 +18,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s1 = "";
 	}
-
 	if (s2 == NULL)
 	{
 		s2 = "";
 	}
-
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 	}
-
 	for (j = 0; s2[j] != '\0'; j++)
 	{
 	}
-	
 	if (n < j)
 	{
 		j = n;
 	}
-
 	conc = malloc(sizeof(char) * (i + j) + 1);
-
 	if (conc == NULL)
 	{
 		return (NULL);
 	}
-
 	for (k = 0; k < i; k++)
 	{
 		conc[k] = s1[k];
