4f0938c787f6dd721525839d3a159742075e629e
NMenendez24 <nahuelmt24@gmail.com>
2022-10-27 07:43:22 -0700

    Task-1-Fix

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include "dog.h"
 
-
+/**
+ * init_dog - Initialize a Structure
+ * Return: Nothing
+ * @d: Structure
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
 
