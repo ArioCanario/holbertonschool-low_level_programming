07e2f6d768ce12558299a72e9815d4425a7cc292
NMenendez24 <nahuelmt24@gmail.com>
2022-10-31 10:14:53 -0700

    Task-3-FinalFix?

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -12,41 +12,41 @@ void print_all(const char * const format, ...)
 {
 	va_list args;
 	int i, n = 0, check;
-	char c;
+	char c, *s;
 	float f;
-	char *s;
 
 	va_start(args, format);
-	while (format[n] != '\0')
+	while (format != NULL)
 	{
-		check = 0;
-		switch (format[n])
+		while (format[n] != '\0')
 		{
-			case 'c':
+			check = 0;
+			switch (format[n])
+			{
+				case 'c':
 				{	c = va_arg(args, int);
 					printf("%c", c);
 					check = 1;
 					break; }
-			case 'i':
+				case 'i':
 				{	i = va_arg(args, int);
 					printf("%d", i);
 					check = 1;
 					break; }
-			case 'f':
+				case 'f':
 				{	f = va_arg(args, double);
 					printf("%f", f);
 					check = 1;
 					break; }
-			case 's':
+				case 's':
 				{	s = va_arg(args, char *);
 					if (s == NULL)
 						s = "(nil)";
 					printf("%s", s);
-					check = 1;
-					break; } }
-		if (format[n + 1] != '\0' && check == 1)
-			printf(", ");
-		n++; }
-	printf("\n");
-	va_end(args);
+					check = 1; } }
+			if (format[n + 1] != '\0' && check == 1)
+				printf(", ");
+			n++; } }
+		printf("\n");
+		va_end(args);
 }
