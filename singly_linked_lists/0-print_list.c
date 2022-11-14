5eef4b12cce1e6dec8201131021bb061432bd685
NMenendez24 <nahuelmt24@gmail.com>
2022-11-14 17:39:30 -0300

    Task 0 Betty Fix

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,13 +2,18 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * print_list - Prints values of a list
+ * Return: Number of nodes
+ * @h: Pointer to nodes
+ */
 size_t print_list(const list_t *h)
 {
 	int i = 0;
 
 	for (; h != NULL; h = h->next, i++)
 	{
-		if(h->str != NULL)
+		if (h->str != NULL)
 			printf("[%d] %s", h->len, h->str);
 		else
 		{
@@ -16,34 +21,5 @@ size_t print_list(const list_t *h)
 		}
 		printf("\n");
 	}
-	return(i);
-}
-
-
-
-
-
-
-
-
-
-
-/**
- * _strlen - returns the length of a string
- * @s: pinter to addres os strign
- *
- * Return: all elements of the stirng
- */
-int _strlen(char *s)
-{
-        int count = 0;
-
-        for (; s[count]; count++)
-                ;
-        return (count);
-}
-
-int _puts(char *str)
-{
-        return (write(1, str, _strlen(str)));
+	return (i);
 }
