a33379931c8220a4e24a2fc0db0ac2c679da3b28
NMenendez24 <nahuelmt24@gmail.com>
2022-11-16 16:58:31 -0300

    Task-5

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -12,9 +12,23 @@ void free_listint2(listint_t **head)
 {
 	if (head)
 	{
-		free_listint2(head->next);
-		free(head);
+		free_listint(*head);
+		*head = NULL;
 	}
-	*head = NULL;
 }
 
+/**
+ * free_listint - Frees a list
+ * Return: Nothing
+ * @head: Pointer to the first node of a list
+ */
+
+
+void free_listint(listint_t *head)
+{
+	if (head)
+	{
+		free_listint(head->next);
+		free(head);
+	}
+}
