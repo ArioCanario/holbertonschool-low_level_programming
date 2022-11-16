11f2a38467c48b9505f3777e71a625bdf7c9d9d9
NMenendez24 <nahuelmt24@gmail.com>
2022-11-16 17:24:39 -0300

    Task-7

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
-	for (; head; head = head->next, n++)
+	for (; head; head = head->next, i++)
 	{
 		if (i == index)
 		{
