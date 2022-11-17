0c7256016be6fef45d472e5c4a65417fe67261dc
NMenendez24 <nahuelmt24@gmail.com>
2022-11-17 08:55:30 -0800

    Task-0-Fix

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -13,12 +13,12 @@ unsigned int binary_to_uint(const char *b)
 	int len = _strlen(b) - 1;
 	unsigned int num = 0;
 
-	for (; i >= 0; i--)
+	for (;b && i >= 0; i--)
 	{
 		if (b[i] == '1')
-		{
 			num += calc(i, len);
-		}
+		else if(b[i] != '0' || b[i] != '1')
+			return (0);
 	}
 	return (num);
 }
