95ee1756c0de93c33c593e0e40f3fec3dcd1c8cb
NMenendez24 <nahuelmt24@gmail.com>
2022-11-20 00:31:53 -0300

    Task-5-Fix

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -17,4 +17,8 @@ int set_bit(unsigned long int *n, unsigned int index);
 
 int clear_bit(unsigned long int *n, unsigned int index);
 
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+unsigned int counter(unsigned long int a, unsigned long int b);
+
 #endif
