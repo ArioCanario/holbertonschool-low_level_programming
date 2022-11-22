ef56719d67f560a0d52a9907a51c72cd2cc6a782
NMenendez24 <nahuelmt24@gmail.com>
2022-11-22 07:42:19 -0800

    Task-1-Fix

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 
 	c = _strlen(text_content);
 
-	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 
 	if (fd == -1)
 		return (-1);
