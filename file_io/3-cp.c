49247e76c955b1e2239fcf24ef84ea39cc0e38b6
NMenendez24 <nahuelmt24@gmail.com>
2022-11-22 10:50:36 -0800

    task-3-fix

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -40,17 +40,18 @@ void cp(char *filename_old, char *filename_new)
 	while (r != 0)
 	{
 		w = write(fd_new, buffer, r);
-		if (w == -1)
+		if (w == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename_new);
 			exit(99);
 		}
 		r = read(fd_old, buffer, 1024);
-		if (r == -1)
+	/*	if (r == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename_old);
 			exit(98);
 		}
+		*/
 	}
 	c_old = close(fd_old);
 	if (c_old == -1)
