799b957ea090526d86e9a6d4d6963608ac2e5a76
NMenendez24 <nahuelmt24@gmail.com>
2022-11-23 08:19:21 -0800

    Task-7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -25,7 +25,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		hn->next = *h;
 		*h = hn;
-		return (h);
+		return (hn);
 	}
 	for (; tmp; tmp = tmp->next, i++)
 	{
