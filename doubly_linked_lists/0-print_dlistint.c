a0a9328652930772ff33e45a8718830c103bcb41
NMenendez24 <nahuelmt24@gmail.com>
2022-11-23 05:57:40 -0800

    Task-0

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * print_dlistint - Prints values of a list
+ * Return: Number of nodes
+ * @h: Pointer to nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int i = 0;
+
+	for (; h != NULL; h = h->next, i++)
+	{
+		if (h->n)
+			printf("%d", h->n);
+		printf("\n");
+	}
+	return (i);
+}
