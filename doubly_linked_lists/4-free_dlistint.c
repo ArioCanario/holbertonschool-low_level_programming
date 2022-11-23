195d7d8085d15971f46201642fafac8ffb8064cb
NMenendez24 <nahuelmt24@gmail.com>
2022-11-23 07:50:08 -0800

    Task-4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * free_dlist - Frees a list
+ * Return: Nothing
+ * @head: Head
+ */
+
+void free_dlist(dlistint_t *head)
+{
+	if (head)
+	{
+		free_dlist(head->next);
+		free(head);
+	}
+}
