5e75fc04b9809cf310a3a30db0af6b8f82b152d7
NMenendez24 <nahuelmt24@gmail.com>
2022-11-24 14:49:21 -0300

    Task-7-Fix

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -35,7 +35,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	while ((tmp->next != NULL) && (idx > i + 1))
 	{
 		tmp = tmp->next;
-		i++
+		i++;
 	}
 	if (idx > i)
 		return (NULL);
