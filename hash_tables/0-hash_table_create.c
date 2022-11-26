ca04da3b9a8c82630c4d3e19c5525feddc8e4456
NMenendez24 <nahuelmt24@gmail.com>
2022-11-26 12:19:36 -0300

    Task-0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!table)
 		return (NULL);
 	table->size = size;
-	node = malloc(sizeof(hash_node_t));
+	node = malloc(size * sizeof(hash_node_t));
 	if (!node)
 	{
 		free(table);
