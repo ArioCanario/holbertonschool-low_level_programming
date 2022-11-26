d99998e249005da17129ba1655e162f846926051
NMenendez24 <nahuelmt24@gmail.com>
2022-11-26 12:18:02 -0300

    Task-0

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table
+ * Return: Pointer to the hash table in success, NULL if it fails
+ * @size: Size of the hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	hash_node_t **node;
+
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL);
+	table->size = size;
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+	{
+		free(table);
+		return (NULL);
+	}
+	table->array = node;
+	return (table);
+}
