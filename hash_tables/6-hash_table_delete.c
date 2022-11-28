da4c76967377dc433f2793243f77762a2b636d89
NMenendez24 <nahuelmt24@gmail.com>
2022-11-27 22:50:23 -0300

    Task-6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table
+ * Return: Nothing
+ * @ht: Hash Table
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int c;
+	hash_node_t *d, *tmp = NULL;
+
+	if (!ht)
+		return;
+	for (c = 0; c < ht->size; c++)
+	{
+		d = ht->array[c];
+		while (d)
+		{
+			tmp = d->next;
+			free(d->key);
+			free(d->value);
+			free(d);
+			d = tmp;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
