1d5efa0386336fb07be33433baf3c20274d740b2
NMenendez24 <nahuelmt24@gmail.com>
2023-01-06 15:15:38 -0300

    Task-0-Betty

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,19 +1,27 @@
 #include "search_algos.h"
 #include <stdio.h>
 
+/**
+ * linear_search - Searchs an integer in an array
+ * Return: Index in success, -1 if it fails
+ * @size: Size of the array
+ * @value: Value to search
+ * @array: Array
+ */
+
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
-	if(!array)
-		return(-1);
+	if (!array)
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if(array[i] == value)
+		if (array[i] == value)
 		{
-			return(i);
+			return (i);
 		}
 	}
-	return(-1);
+	return (-1);
 }
